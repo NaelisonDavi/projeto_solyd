fd66ef66214beb8b945050e8943ed222757251fe
NaelisonDavi <n.davi.c.oliveira@gmail.com>
2023-02-08 18:07:09 -0300

    #1 aula 7

---
+++ aula7.py
@@ -0,0 +1,21 @@
+def maior_valor(lista):
+    return max(lista)
+
+def menor_valor(lista):
+    return min(lista)
+
+if __name__ == '__main__':
+
+    lista = []
+
+    
+    lista.append(input('digite o primeiro valor para a lista: '))
+    lista.append(input('digite o segundo valor para a lista: '))
+    lista.append(input('digite o terceiro valor para a lista: '))
+    lista.append(input('digite o quarto valor para a lista: '))
+    lista.append(input('digite o quinto valor para a lista: '))
+
+
+        
+print(maior_valor(lista))
+print(menor_valor(lista))
